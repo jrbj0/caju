1022edaec877d88caae22b40c80f26558f5ad1fd
Ewerton <effs@cin.ufpe.br>
2019-10-19 13:58:06 -0300

    estruturação do app

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: LandingPageComponent }
+  { path: '', component: LandingPageComponent }
 ];
 
 @NgModule({
--- web/src/app/core/core.module.ts
+++ web/src/app/core/core.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
-import { LandingPageComponent } from '../landing-page/landing-page.component';
+import { LandingPageComponent } from '../components/landing-page/landing-page.component';
+
 
 @NgModule({
   declarations: [
