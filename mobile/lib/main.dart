470e445a7bd27b2643eb3b3ed272e6c097f8effa
Ricardo Ebbers <recl@cin.ufpe.br>
2019-10-26 15:44:35 -0300

    Basic app skeleton

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -7,14 +7,50 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      theme: ThemeData(
+        primaryColor: Colors.teal
+      ),
+      
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          title: Text('KAJU', style: TextStyle(color: Colors.teal, fontWeight: FontWeight.bold),),
+          actions: <Widget>[
+            Padding(
+              padding:EdgeInsets.only(right: 15.0),
+              child: CircleAvatar(backgroundColor: Colors.grey,))
+          ],
         ),
         body: Center(
           child: Container(
-            child: Text('Hello World'),
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Placeholder(),
+            ),
           ),
+        ),
+        bottomNavigationBar: Row(
+          children: <Widget>[
+          RaisedButton.icon(
+            icon: Icon(Icons.mic),
+            label: Text("Áudio"),
+            onPressed: () {},
+            color: Colors.teal,
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          ),
+            RaisedButton.icon(
+            icon: Icon(Icons.videocam),
+            label: Text("Vídeo"),
+            onPressed: () {},
+            color: Colors.teal,
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          ),
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         ),
       ),
     );
