1e1244846b6645b2fe5769f88462080abc8da4d4
Ewerton <effs@cin.ufpe.br>
2019-11-02 09:59:29 -0300

    flexLayout funcionando + componente project-card criado (n implementado)

--- web/src/app/core/core.module.ts
+++ web/src/app/core/core.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { LandingPageComponent } from '../landing-page/landing-page.component';
+import { ProjectCardComponent } from '../project-card/project-card.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    ProjectCardComponent
   ],
   imports: [
     CommonModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    FlexLayoutModule
   ]
 })
 export class CoreModule { }
