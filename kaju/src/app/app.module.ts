f1aea3966a0373a807d5074e538da054c6a3178c
José Reginaldo <jrbj@cin.ufpe.br>
2019-12-01 19:31:08 -0300

    initial code to login

--- kaju/src/app/app.module.ts
+++ kaju/src/app/app.module.ts
@@ -4,15 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuftbBakpdrJzYXOr0L9CkX2nEJF5_8G8",
+  authDomain: "kaju-bc449.firebaseapp.com",
+  databaseURL: "https://kaju-bc449.firebaseio.com",
+  projectId: "kaju-bc449",
+  storageBucket: "kaju-bc449.appspot.com",
+  messagingSenderId: "536697512595",
+  appId: "1:536697512595:web:af5d3454af6c4c6e76e366",
+  measurementId: "G-YVCKVRL4VP"
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AngularFireModule, AngularFirestoreModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
