1cd36d48a1ca6950527644f276e002686a4f3b63
José Reginaldo <jrbj@cin.ufpe.br>
2019-12-12 15:19:23 -0300

    add highlight

--- kaju/src/app/components/transcription/transcription-view/transcription-view.component.ts
+++ kaju/src/app/components/transcription/transcription-view/transcription-view.component.ts
@@ -21,13 +21,25 @@ export class TranscriptionViewComponent implements OnInit {
     return (this.selectedTranscription.confidence*100).toFixed(2)
   }
   get ocurrences(){
-    return this.selectedTranscription.transcript.split(this.filterWord).length-1
+    return this.selectedTranscription.transcript.toLowerCase().split(this.filterWord.toLowerCase()).length-1
   }
   get showOcurrences(){
     if(this.filterWord){
       return this.filterWord.length>0 && this.ocurrences>0
     }
     return false
+  }
+  get transcription(){
+    if(!this.filterWord) return this.selectedTranscription.transcript
+    if(this.filterWord.length===0)return this.selectedTranscription.transcript
+    let text=this.selectedTranscription.transcript
+    var idx = text.toLowerCase().indexOf(this.filterWord.toLowerCase());
+    while (idx != -1) {
+      text = text.substring(0,idx) + "<span class='highlight'>" + text.substring(idx,idx+this.filterWord.length) + "</span>" + text.substring(idx+this.filterWord.length, text.length);
+
+      idx = text.toLowerCase().indexOf(this.filterWord.toLowerCase(), idx + 30);
+    }
+    return text
   }
 
 }
